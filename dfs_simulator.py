7d0e8bf3cc6900731418cc1ca1ff53b435f99398
bilalmahmood1 <bilal.mahmood.engineer@gmail.com>
2022-02-19 00:26:26 +0100

    Added lineup and statistics file mismatching feature

--- dfs_simulator.py
+++ dfs_simulator.py
@@ -6,10 +6,11 @@ import numpy as np
 from PIL import Image
 from numpy.random import normal
 import base64
-
+from fuzzywuzzy import fuzz
 
 ## Set up seed for testing
-np.random.seed(1)
+## np.random.seed(1)
+
 
 ## Helper functions
 @st.cache
@@ -27,34 +28,56 @@ st.set_page_config(
     }
 )
 
+
+def find_potential_matches(player_info_database, name, n = 5):
+    candidates = sorted([(fuzz.token_sort_ratio(key, name), key) for key in player_info_database], reverse = True)[:n]
+    return [n[1] for n in candidates]
+
+def find_players_lineups(lineups_database):
+    """Find all the names of the players in the lineups"""
+    lineup_players = set()
+    for i in lineups_database:
+        lineup_players = lineup_players.union(set(i["player_names"]))
+    return list(lineup_players)
+
+
+def find_player_in_the_mapping_table(df_names, name):
+    """Find all the player names in the names mapping table"""
+    return list(df_names[df_names["lineup name"] == name]["statistics name"].values)
+
+
 ## Title and logo Image
 st.title('DFS Simulator')
 image = Image.open('images/logo.png')
 st.image(image, use_column_width = False, caption='Monte Carlo Simulation')
 
-## Total entries
-total_entries = st.sidebar.number_input('Number of Total Entries', min_value = 1, value = 359281)
-st.write('Total Entries: ', total_entries)
 
-
-## Number of simulatiorn
-number_sims = st.sidebar.number_input('Number of simulations', min_value = 1, max_value = 10000, step = 1, value =  2000)
+## Number of simulations
+number_sims = st.sidebar.number_input('Number of simulations', min_value = 1, max_value = 50000, step = 1, value =  2000)
 st.write('Total Simulations: ', number_sims)
+
+## Display intermediate results
+display = st.sidebar.radio("Show intermediate results?",('No', 'Yes'))
+
 
 ## Upload files
 st.sidebar.header("Simulation Inputs")
-# Statsitics
+# Statistics
 uploaded_file_statistics = st.sidebar.file_uploader("Upload Player Statistics", type=["csv"])
 if uploaded_file_statistics is not None:
     df_fd = pd.read_csv(uploaded_file_statistics)
     st.subheader("Player Statistics")
-    st.write("There are %d rows and %d columns in Player statisitcs file" %(df_fd.shape[0],df_fd.shape[1]))
+    st.write("There are %d rows and %d columns in Player statistics file" %(df_fd.shape[0],df_fd.shape[1]))
     st.write(df_fd)
 
 # Lineups 
 uploaded_file_lineups = st.sidebar.file_uploader("Upload Lineups", type=["csv"])
 if uploaded_file_lineups is not None:
     df_crunch = pd.read_csv(uploaded_file_lineups)
+    ## Total lineups
+    total_entries = df_crunch.shape[0]
+    st.write('Total Entries: ', total_entries)
+
     st.subheader("Lineups")
     st.write("There are %d rows and %d columns in Lineups file" %(df_crunch.shape[0],df_crunch.shape[1]))
     st.write(df_crunch)
@@ -63,11 +86,20 @@ if uploaded_file_lineups is not None:
 uploaded_file_payouts = st.sidebar.file_uploader("Upload Payouts", type=["csv"])
 if uploaded_file_payouts is not None:
     df_payouts = pd.read_csv(uploaded_file_payouts)
-    st.subheader("Payout structure")
+    st.subheader("Payout Structure")
     st.write("There are %d rows and %d columns in Payout file" %(df_payouts.shape[0],df_payouts.shape[1]))
     st.write(df_payouts) 
 
+# Names
+uploaded_file_payouts = st.sidebar.file_uploader("Upload Name Mappings", type=["csv"])
+if uploaded_file_payouts is not None:
+    df_names = pd.read_csv(uploaded_file_payouts)
+    st.subheader("Name Mappings")
+    st.write("There are %d rows and %d columns in Name Mappings file" %(df_names.shape[0],df_names.shape[1]))
+    st.write(df_names) 
+
 if st.button('Run Simulation'):
+
     ## Creating player statistics database
     player_info_database = {}
     for player in df_fd.iterrows():
@@ -89,21 +121,42 @@ if st.button('Run Simulation'):
         lineups_database.append(data)
         lineup_number = lineup_number + 1 
 
+
+    ## Resolve name mismatching
+    lineup_players = find_players_lineups(lineups_database)
+
+    for name in lineup_players:
+        ## Already information available in the player_info_db
+        if name in player_info_database:
+            pass
+        else:
+            potential_players = find_player_in_the_mapping_table(df_names, name)
+            found = False
+            for player in potential_players:
+                if player in player_info_database:
+                    player_info_database[name] = player_info_database[player]
+                    found = True
+            if not found:
+                st.write(name, " not found")
+                st.write("Potential mappings that you can put in the mappings file")
+                candidates = find_potential_matches(player_info_database, name, 3)
+                for c in candidates:
+                    st.write(name, " --> ", c)
+              
     ## Find the total ownership
     for i in range(len(lineups_database)):
         prod_ownership = float(1)
         for name in lineups_database[i]["player_names"]:
-
-            prod_ownership *= player_info_database[name]["ownership"]
-         
+            prod_ownership *= player_info_database[name]["ownership"] 
         lineups_database[i]["prod_ownership"] = prod_ownership
+
+
 
     ## Normalize ownership
     total_ownerhip = np.sum([i["prod_ownership"] for i in lineups_database])
     for i in range(len(lineups_database)):
         lineups_database[i]["norm_prod_ownership"] =  lineups_database[i]["prod_ownership"] / total_ownerhip
         lineups_database[i]["duplication"] = lineups_database[i]["norm_prod_ownership"] * total_entries
-
 
 
     ## Create lineup statistics database
@@ -113,10 +166,8 @@ if st.button('Run Simulation'):
     df_result["norm prod ownership"] = [lineup["norm_prod_ownership"] for lineup in lineups_database]
     df_result["duplication"] = [lineup["duplication"] for lineup in lineups_database]
 
-    ## Line duplication set to 1
-    ## df_result["duplication"] = 1
 
-    st.write("Lineup statistics")
+    st.subheader("Lineup statistics")
     st.write(df_result.style.format({"prod ownership" :"{:.9f}", "norm prod ownership": "{:.7f}", "duplication": "{:.7f}"}))
 
 
@@ -143,14 +194,22 @@ if st.button('Run Simulation'):
 
         df_sims.columns = ["Sim " + str(i) for i in range(1, number_sims + 1)]
 
-        st.write("Simulations")
-        st.write(df_sims)
+        
+        if display == "Yes":
+            st.subheader("Simulations")
+            st.write(df_sims)
+
+        st.success('Successfully Ran the Simulations')
 
 
         ## Ranking the simulation
         df_sims_ranks = df_sims.rank(axis=0, ascending= False, method="min").apply(np.int64)
-        st.write("Ranked Simulations")
-        st.write(df_sims_ranks)
+
+        if display == "Yes":
+            st.subheader("Ranked Simulations")
+            st.write(df_sims_ranks)
+            
+        st.success('Successfully Ranked the Simulations')
 
         ## Line up duplication
         df_duplications = pd.DataFrame([(l["lineup"],l["duplication"]) for l in lineups_database],
@@ -187,10 +246,11 @@ if st.button('Run Simulation'):
         df_final_result_downloads["Expected Money"] = df_final_result["Expected Money"]
 
 
-        st.write("Ranked SIM Lineups")
-        st.write(df_final_result_downloads)
+        if display == "Yes":
+            st.subheader("Ranked SIM Lineups")
+            st.write(df_final_result_downloads)
 
-        st.success('Successfully Simulated the Lineups')
+        st.success('Successfully Ranked the Prize/duplication Lineups')
         ## Save the ranked simulation file
         csv = convert_df(df_final_result_downloads)
         st.download_button(
